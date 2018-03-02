c30f793e579fdc36dc3271792a6cd0ae4b093aeb
Matt Trussell <smtrussell@gmail.com>
2018-03-02 01:04:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,5 +4,17 @@ class App < Sinatra::Base
 		erb :index
 	end
 
+	get '/hello.erb' do
+		erb :index
+	end
+
+	get '/goodbye.erb' do
+		erb :index
+	end
+
+	get '/date.erb' do
+		erb :index
+	end
+
 
 end
